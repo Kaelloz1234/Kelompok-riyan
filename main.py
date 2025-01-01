2a26ff57538eab57b307e6acf8df465ed6c3d623
yan <hpitono24@gmail.com>
2025-01-01 18:03:11 +0700

    Add files via upload

---
+++ main.py
@@ -0,0 +1,9 @@
+from gui import CalculatorGui
+
+class Main:
+    def __init__(self):
+        calculatorgui = CalculatorGui()
+        calculatorgui.init_calc_window()
+    
+if __name__ == "__main__":
+    Main()
