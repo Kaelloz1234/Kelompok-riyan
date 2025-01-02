1c005de3f11f5b5535085cf2947381adedf5817c
yan <hpitono24@gmail.com>
2025-01-02 14:11:58 +0700

    calculate.py

--- calculate.py
+++ calculate.py
@@ -1,225 +1,124 @@
 import math
 
-class Calculator:
+class Calculator: # buat class untuk diimport di gui.py
     def __init__(self):
-
-        self.standardCalcCharacters = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 
-                                    '+', '-', '*', '/', '.']
+        # ini variabel apa aja buat kalkulator scientificnya
+        self.standardCalcCharacters = list('0123456789+-*/.')
         self.operatorCharacters = ['+', '-', '*', '/', '=', 'x!', '%']
+        self.constantChars = ['π', 'e']
+        self.operator, self.operatorDisplay = [], []
+        self.oldBtnValue, self.oldBtnValueForDisplay, self.answer = '', '', ''
 
-        self.constantChars = ["π", "e"]
-
-        self.operator = [] 
-        self.operatorDisplay = [] 
-        self.oldBtnValue = '' 
-        self.oldBtnValueForDisplay = '' # tampilan
-        self.answer = '' # hasil 
-
+    # fungsi untuk input tombol dan memperbarui tampilan
     def btn_click_func(self, btnValue, textInput):
-        
-        # jika nilai btn sebelumnya "=" dan nilai btn sekarang di operatorCharacters, [result]
-        # jkalo ga gini nanti yang di atas gak jalan
-        if self.oldBtnValue == "=" and btnValue in self.operatorCharacters:
-
-            self.operator = [self.answer]
-            self.operatorDisplay = [self.answer]
-        
-        if self.oldBtnValue == "=" and btnValue not in self.operatorCharacters:
+        # memeriksa apakah input sebelumnya itu '=' terus mengoperasikan input berikutnya
+        if self.oldBtnValue == '=' and btnValue in self.operatorCharacters:
+            self.operator, self.operatorDisplay = [self.answer], [self.answer]
+        elif self.oldBtnValue == '=' and btnValue not in self.operatorCharacters:
             self.operatorDisplay = []
-        
+
+        # menggabungkan semua operator yang dimasukkin
+        operatorStr = ''.join(self.operator)
+
+        # ini button mapping scientifcnya supaya berfungsi 
+        btn_map = {
+            '√': ('math.sqrt(', '√'),
+            'cos': ('math.cos(math.radians(', 'cos'),
+            'sin': ('math.sin(math.radians(', 'sin'),
+            'tan': ('math.tan(math.radians(', 'tan'),
+            'cosh': ('math.cosh(math.radians(', 'cosh'),
+            'sinh': ('math.sinh(math.radians(', 'sinh'),
+            'tanh': ('math.tanh(math.radians(', 'tanh'),
+            'exp': ('math.exp(', 'exp'),
+            'log': ('math.log(', 'log'),
+            'log10': ('math.log10(', 'log10')
+        }
+
+        # ini buat input button kalkukator scientificnya
         if btnValue in self.standardCalcCharacters:
-            
-            self.oldBtnValue = btnValue
-            self.oldBtnValueForDisplay = btnValue
-            self.operator.append(self.oldBtnValue)
-            self.operatorDisplay.append(self.oldBtnValueForDisplay)
-        else:
-            operatorStr = ''.join(self.operator)
-            if btnValue == '√':
-                if operatorStr.isdigit() or self.oldBtnValue.isdigit() or self.oldBtnValueForDisplay in self.constantChars:
-                    self.oldBtnValue = '*math.sqrt('
-                    self.oldBtnValueForDisplay = '*√'
-                else:
-                    self.oldBtnValue = 'math.sqrt('
-                    self.oldBtnValueForDisplay = '√'
-                
-                self.operator.append(self.oldBtnValue)
-                self.operatorDisplay.append(self.oldBtnValueForDisplay)
-            # sqrt √
-            elif btnValue == 'cos':
-                if operatorStr.isdigit() or self.oldBtnValue.isdigit() or self.oldBtnValueForDisplay in self.constantChars:
-                    self.oldBtnValue = '*math.cos(math.radians('
-                    self.oldBtnValueForDisplay = '*cos'
-                else:
-                    self.oldBtnValue = 'math.cos(math.radians('
-                    self.oldBtnValueForDisplay = 'cos'
-                
-                self.operator.append(self.oldBtnValue)
-                self.operatorDisplay.append(self.oldBtnValueForDisplay)
-            elif btnValue == 'tan':
-                if operatorStr.isdigit() or self.oldBtnValue.isdigit() or self.oldBtnValueForDisplay in self.constantChars:
-                    self.oldBtnValue = '*math.tan(math.radians('
-                    self.oldBtnValueForDisplay = '*tan'
-                else:
-                    self.oldBtnValue = 'math.tan(math.radians('
-                    self.oldBtnValueForDisplay = 'tan'
-                
-                self.operator.append(self.oldBtnValue)
-                self.operatorDisplay.append(self.oldBtnValueForDisplay)
-            elif btnValue == 'sin':
-                if operatorStr.isdigit() or self.oldBtnValue.isdigit() or self.oldBtnValueForDisplay in self.constantChars:
-                    self.oldBtnValue = '*math.sin(math.radians('
-                    self.oldBtnValueForDisplay = '*sin'
-                else:
-                    self.oldBtnValue = 'math.sin(math.radians('
-                    self.oldBtnValueForDisplay = 'sin'
-                
-                self.operator.append(self.oldBtnValue)
-                self.operatorDisplay.append(self.oldBtnValueForDisplay)
-            elif btnValue == 'cosh':
-                if operatorStr.isdigit() or self.oldBtnValue.isdigit() or self.oldBtnValueForDisplay in self.constantChars:
-                    self.oldBtnValue = '*math.cosh(math.radians('
-                    self.oldBtnValueForDisplay = '*cosh'
-                else:
-                    self.oldBtnValue = 'math.cosh(math.radians('
-                    self.oldBtnValueForDisplay = 'cosh'
-                
-                self.operator.append(self.oldBtnValue)
-                self.operatorDisplay.append(self.oldBtnValueForDisplay)
-            elif btnValue == 'tanh':
-                if operatorStr.isdigit() or self.oldBtnValue.isdigit() or self.oldBtnValueForDisplay in self.constantChars:
-                    self.oldBtnValue = '*math.tanh(math.radians('
-                    self.oldBtnValueForDisplay = '*tanh'
-                else:
-                    self.oldBtnValue = 'math.tanh(math.radians('
-                    self.oldBtnValueForDisplay = 'tanh'
-                
-                self.operator.append(self.oldBtnValue)
-                self.operatorDisplay.append(self.oldBtnValueForDisplay)
-            elif btnValue == 'sinh':
-                if operatorStr.isdigit() or self.oldBtnValue.isdigit() or self.oldBtnValueForDisplay in self.constantChars:
-                    self.oldBtnValue = '*math.sinh(math.radians('
-                    self.oldBtnValueForDisplay = '*sinh'
-                else:
-                    self.oldBtnValue = 'math.sinh(math.radians('
-                    self.oldBtnValueForDisplay = 'sinh'
-                
-                self.operator.append(self.oldBtnValue)
-                self.operatorDisplay.append(self.oldBtnValueForDisplay)
-            elif btnValue == 'exp':
-                if operatorStr.isdigit() or self.oldBtnValue.isdigit() or self.oldBtnValueForDisplay in self.constantChars:
-                    self.oldBtnValue = '*math.exp('
-                    self.oldBtnValueForDisplay = '*exp'
-                else:
-                    self.oldBtnValue = 'math.exp('
-                    self.oldBtnValueForDisplay = 'exp'
-                
-                self.operator.append(self.oldBtnValue)
-                self.operatorDisplay.append(self.oldBtnValueForDisplay)
-            elif btnValue == 'log':
-                if operatorStr.isdigit() or self.oldBtnValue.isdigit() or self.oldBtnValueForDisplay in self.constantChars:
-                    self.oldBtnValue = '*math.log('
-                    self.oldBtnValueForDisplay = '*log'
-                else:
-                    self.oldBtnValue = 'math.log('
-                    self.oldBtnValueForDisplay = 'log'
-                
-                self.operator.append(self.oldBtnValue)
-                self.operatorDisplay.append(self.oldBtnValueForDisplay)
-            elif btnValue == 'log10':
-                if operatorStr.isdigit() or self.oldBtnValue.isdigit() or self.oldBtnValueForDisplay in self.constantChars:
-                    self.oldBtnValue = '*math.log10('
-                    self.oldBtnValueForDisplay = '*log10'
-                else:
-                    self.oldBtnValue = 'math.log10('
-                    self.oldBtnValueForDisplay = 'log10'
-                
-                self.operator.append(self.oldBtnValue)
-                self.operatorDisplay.append(self.oldBtnValueForDisplay)
-            # ex: 8%3 = 2
-            elif btnValue == 'mod':
-                self.oldBtnValue = btnValue
-                self.oldBtnValueForDisplay = btnValue
-                self.operator.append("%")
-                self.operatorDisplay.append("%")
-            elif btnValue == 'π':
-                if operatorStr.isdigit():
-                    self.oldBtnValue = '*math.pi'
-                    self.oldBtnValueForDisplay = '*π'
-                else:
-                    self.oldBtnValue = 'math.pi'
-                    self.oldBtnValueForDisplay = 'π'
+            self._append_operator(btnValue)
+        elif btnValue in btn_map:
+            func, display = btn_map[btnValue]
+            self._append_function(func, display, operatorStr)
+        elif btnValue == 'mod':
+            self._append_operator('%')
+        elif btnValue == 'π':
+            self._append_constant('math.pi', 'π', operatorStr)
+        elif btnValue == 'e':
+            self._append_constant('math.e', 'e', operatorStr)
+        elif btnValue == '%':
+            self._append_percentage(operatorStr)
+        elif btnValue == 'x!':
+            self._append_factorial(operatorStr)
+        elif btnValue == '±':
+            self._append_negate(operatorStr)
+        elif btnValue == '=':
+            self._calculate_result(operatorStr)
+        elif btnValue == 'C':
+            self._clear_last_entry()
+        elif btnValue == 'CE':
+            self._clear_all()
 
-                self.operator.append(self.oldBtnValue)
-                self.operatorDisplay.append(self.oldBtnValueForDisplay)
-            elif btnValue == 'e':
-                if operatorStr.isdigit():
-                    self.oldBtnValue = '*math.e'
-                    self.oldBtnValueForDisplay = '*e'
-                else:
-                    self.oldBtnValue = 'math.e'
-                    self.oldBtnValueForDisplay = 'e'
-            
-                self.operator.append(self.oldBtnValue)
-                self.operatorDisplay.append(self.oldBtnValueForDisplay)
-            elif btnValue == '%':
-                if "(" in operatorStr: 
-                    num_parentheses = operatorStr.count('(')
-                    self.operator.append(num_parentheses * ")")
-                    operatorStr = ''.join(self.operator) # nyabung ke string baru
+        # ini buat mengupdate tampilannya
+        textInput.set(''.join(self.operatorDisplay))
 
-                self.oldBtnValue = operatorStr + "*0.01"
-                self.oldBtnValueForDisplay = ''.join(self.operatorDisplay) + "%" # display
-            
-                self.operator = [self.oldBtnValue]
-                self.operatorDisplay = [self.oldBtnValueForDisplay]
+    # menambahkan angka ke daftar operator sama display
+    def _append_operator(self, value):
+        self.oldBtnValue = self.oldBtnValueForDisplay = value
+        self.operator.append(value)
+        self.operatorDisplay.append(value)
 
-            elif btnValue == 'x!':
-                if operatorStr.isdigit() or str(eval(operatorStr)).isdigit(): # faktorial buat bilangan bulat aja
-                    self.oldBtnValue = "math.factorial(" + operatorStr
-                    self.oldBtnValueForDisplay = '!'
-            
-                self.operator = [self.oldBtnValue]
-                self.operatorDisplay.append(self.oldBtnValueForDisplay)
-            elif btnValue == '±':
-                if "(" in operatorStr: 
-                    num_parentheses = operatorStr.count('(')
-                    self.operator.append(num_parentheses * ")")
-                    # tutup semua tanda kurung sebelum masukin minus, positif jadi negatif, negatif jadi positif
-                    operatorStr = ''.join(self.operator) 
+    # menambahkan fungsi matematika cos, sin, dan log
+    def _append_function(self, func, display, operatorStr):
+        prefix = '*' if operatorStr.isdigit() else ''
+        self.operator.append(prefix + func)
+        self.operatorDisplay.append(prefix + display)
 
-                self.oldBtnValue = "-" + operatorStr
-                self.oldBtnValueForDisplay = '-' + ''.join(self.operatorDisplay) 
-            
-                self.operator = [self.oldBtnValue]
-                self.operatorDisplay = [self.oldBtnValueForDisplay]
-            elif btnValue == '=':
-                try:
-                    if "(" in operatorStr and ")" not in operatorStr:
-                        num_parentheses = operatorStr.count('(')
-                        self.operator.append(num_parentheses * ")")
-                        operatorStr = ''.join(self.operator) # masuk string baru lg
-                    
-                    self.answer = str(eval(operatorStr))
-                    self.operatorDisplay = [self.answer] # cuma 1 elemen jawaban
-                    self.operator = [] 
-                    self.oldBtnValue = "="
-                except: 
-                    self.operatorDisplay = ["Error"] # cuma 1 elemen jawaban
-                    self.operator = []
-                    self.oldBtnValue = "="
-            elif btnValue == 'C':
-                del self.operator[-1] 
-                del self.operatorDisplay[-1]
-                
-                if self.operator: 
-                    self.oldBtnValue = self.operator[-1]
-                    self.oldBtnValueForDisplay = self.operatorDisplay[-1]
-                else:
-                    self.oldBtnValue = ''
-                    self.oldBtnValueForDisplay = ''
-            elif btnValue == 'CE': # clear
-                self.operator = []
-                self.operatorDisplay = []
-            
-        textInput.set(''.join(self.operatorDisplay)) # buat nampilin
+    # menambahkan konstanta π dan e 
+    def _append_constant(self, func, display, operatorStr):
+        prefix = '*' if operatorStr.isdigit() else ''
+        self.operator.append(prefix + func)
+        self.operatorDisplay.append(prefix + display)
+
+    # ini buat hitung persentase dan dijadikan desimal angkanya
+    def _append_percentage(self, operatorStr):
+        if '(' in operatorStr:
+            self.operator.append(operatorStr.count('(') * ')')
+        self.operator = [operatorStr + '*0.01']
+        self.operatorDisplay = [''.join(self.operatorDisplay) + '%']
+
+    # menambahkan operasi faktorial ke ekspresinya
+    def _append_factorial(self, operatorStr):
+        if operatorStr.isdigit() or str(eval(operatorStr)).isdigit():
+            self.operator = ['math.factorial(' + operatorStr]
+            self.operatorDisplay.append('!')
+
+    # ini negasi(-) untuk mengubah tanda angka
+    def _append_negate(self, operatorStr):
+        if '(' in operatorStr:
+            self.operator.append(operatorStr.count('(') * ')')
+        self.operator = ['-' + operatorStr]
+        self.operatorDisplay = ['-' + ''.join(self.operatorDisplay)]
+
+    # ini buat menghitung dan menampilkan hasilnya
+    def _calculate_result(self, operatorStr):
+        try:
+            if '(' in operatorStr and ')' not in operatorStr:
+                self.operator.append(operatorStr.count('(') * ')')
+            self.answer = str(eval(''.join(self.operator)))
+            self.operatorDisplay = [self.answer]
+            self.operator, self.oldBtnValue = [], '='
+        except:
+            self.operatorDisplay, self.operator, self.oldBtnValue = ['Error'], [], '='
+
+    # ini buat menghapus input yang terakhir
+    def _clear_last_entry(self):
+        if self.operator:
+            self.operator.pop()
+            self.operatorDisplay.pop()
+            self.oldBtnValue = self.operator[-1] if self.operator else ''
+            self.oldBtnValueForDisplay = self.operatorDisplay[-1] if self.operatorDisplay else ''
+
+    # ini buat clear semua inputnya dan mengatur ulang kalkulator
+    def _clear_all(self):
+        self.operator, self.operatorDisplay, self.oldBtnValue, self.oldBtnValueForDisplay = [], [], '', ''
