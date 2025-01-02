6aa54d9aea1050a5f73be79a185f269787b38634
yan <hpitono24@gmail.com>
2025-01-02 14:22:38 +0700

    main.py

--- main.py
+++ main.py
@@ -1,9 +1,12 @@
-from gui import CalculatorGui
+from gui import CalculatorGui # ini buat import library CalculatorGui dari gui.py
 
+# ini class buat manggil fungsi dari gui.py nampilin objek juga
 class Main:
     def __init__(self):
         calculatorgui = CalculatorGui()
         calculatorgui.init_calc_window()
-    
+
+# nah ini buat nampilinnya
 if __name__ == "__main__":
     Main()
+    
