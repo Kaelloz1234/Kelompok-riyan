eaa79470f29e93f0e29e8d6bf6f1367cdbdec4a3
yan <hpitono24@gmail.com>
2025-01-02 14:10:01 +0700

    gui.py

--- gui.py
+++ gui.py
@@ -1,22 +1,22 @@
 from tkinter import *
-from calculate import Calculator
+from calculate import Calculator # ini buat import library Calculator yang ada di calculate.py
 
-class CalculatorGui:
+class CalculatorGui: #ini buat class supaya bisa dipake di main.py
     def __init__(self):
         self.root = Tk()
         self.root.title("Scientific Calculator")
         
-        # Set background color and prevent resizing
-        self.root.configure(background="slategrey")  # Dark background
+        # bikin background
+        self.root.configure(background="dimgrey")  # warna background
         self.root.resizable(width=False, height=False)
-        self.root.geometry("800x600+400+100")  # Adjusted window size
+        self.root.geometry("780x620+400+100")  # background size
 
-        self.calc = Frame(self.root, bg="slategrey")
+        self.calc = Frame(self.root, bg="dimgrey")
         self.calc.grid()
 
         self.textInput = StringVar()
 
-        # Display configuration
+        # display 
         self.txtDisplay = Entry(self.calc, font=('arial', 20, 'bold'), textvariable=self.textInput, 
                                 bg="#ecf0f1", bd=10, width=50, justify=RIGHT)
         self.txtDisplay.grid(row=0, column=0, columnspan=7, pady=20)
@@ -24,7 +24,7 @@ class CalculatorGui:
 
         self.numbers = "789456123"
 
-        # All buttons, including functions and numbers
+        # ini buat menyatakan karakter dari tombolnya
         self.allcharacters = ["C", "CE", "√", "+", "π", "cos", "tan", 
                               "7", "8", "9", "-", "sin", "cosh", "tanh", 
                               "4", "5", "6", "*", "sinh", "exp", "mod",
@@ -36,20 +36,21 @@ class CalculatorGui:
         i = 0
         btn = []
 
-        # Generate buttons dynamically
+        # bikin buttonnya
         for j in range(2, 7):
             for k in range(7):
                 btn_text = self.allcharacters[i]
+                # ini buat button bagian angkanya dan background jika ditekan
                 button = Button(self.calc, text=btn_text, width=5, height=2, font=('arial', 18, 'bold'), 
-                                bd=4, relief=RAISED, bg="royalblue", fg="white", activebackground="gainsboro")
+                                bd=4, relief=RAISED, bg="darkgray", fg="white", activebackground="gainsboro")
                 
                 button.grid(row=j, column=k, padx=10, pady=10)
                 
-                # Style specific buttons
+                # style buat button bagian fungsi
                 if btn_text not in self.numbers:
-                    button.config(bg="navy")  # Non-number buttons have a red color
+                    button.config(bg="darkorange") 
                 
-                # Set button command
+                # set button 
                 button['command'] = lambda x=btn_text: calculator.btn_click_func(x, self.textInput)
                 i += 1
 
